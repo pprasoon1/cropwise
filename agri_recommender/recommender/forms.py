307c351fb002eef81dcec54fd6941d1b0cee8445
Pranay Prasoon <159932934+pprasoon1@users.noreply.github.com>
2024-10-20 20:29:22 +0530

    .

--- agri_recommender/recommender/forms.py
+++ agri_recommender/recommender/forms.py
@@ -6,7 +6,6 @@ class AgriForm(forms.Form):
     state = forms.CharField(max_length=100)
     district = forms.CharField(max_length=100)
     block = forms.CharField(max_length=100, required=False)
-    crop = forms.CharField(max_length=100, required=False)
     start_month = forms.ChoiceField(choices=[(str(i), i) for i in range(1, 13)], required=False)
 
 
